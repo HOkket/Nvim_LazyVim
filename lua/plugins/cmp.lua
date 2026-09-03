7f5e979f59389d4496c70ffdc1688de3bc40294e
Mateus A.M Ferreira <76225268+HOkket@users.noreply.github.com>
2026-09-03 17:18:34 -0300

    Config de cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        documentation = {
+          -- Desativa a abertura automática ao digitar
+          auto_show = false,
+          -- Se preferir automático mas com atraso (ex: 1 segundo), descomente abaixo:
+          -- auto_show = true,
+          -- auto_show_delay_ms = 1000,
+        },
+      },
+      keymap = {
+        -- Define Ctrl + d para abrir/fechar a documentação manualmente
+        ["<C-a>"] = { "show_documentation", "hide_documentation", "fallback" },
+      },
+    },
+  },
+}
